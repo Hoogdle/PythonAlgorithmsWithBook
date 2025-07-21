40d2d2336583867461deeca938e1e0b7fa9685ae
Taeyeong Kim <xodud7737@gmail.com>
2025-07-21 15:59:56 +0900

    _

--- 08.py
+++ 08.py
@@ -2,4 +2,22 @@
 
 ### Caculate quantity of stacked water with given height.
 
-### Solution1 ###
+### Solution1 ### (Two-Pointer)
+def cal_water(wall: list[int])-> int:
+    length = len(wall)
+    left_index, right_index = 0, length-1
+    water = 0
+
+    left_max, right_max = wall[left_index], wall[right_index]
+
+    while left_index < right_index:
+        if left_max < right_max:
+            left_max = max(left_max, wall[left_index])
+            water += left_max - wall[left_index]
+            left_index += 1
+        else:
+            right_max = max(right_max, wall[right_max])
+            water = right_max - wall[right_max]
+            right_max -= 1
+    
+    return water
