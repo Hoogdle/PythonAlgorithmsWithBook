9288d6839914c70e702e51cc6a862f0eb830a9a9
Taeyeong Kim <xodud7737@gmail.com>
2025-07-23 20:33:23 +0900

    _

--- 11.py
+++ 11.py
@@ -4,5 +4,27 @@
 # Make a Alogirithm with O(n) Complexity.
 
 ### Solution1 ###
+
+import collections
+import numpy as np
+
 def mul_without_itself(nums: list[int])-> list[int]:
+    from_left = [1]
+    from_right = collections.deque([1])    
+    length = len(nums)
+
+    for i in range(length-1):
+        left_value = nums[i] * from_left[len(from_left)-1]
+        from_left.append(left_value)
+
+    for i in range(length-1):
+        reversed_i = length - (i+1)
+        right_value = nums[reversed_i] * from_right[0]
+        from_right.appendleft(right_value)
     
+    np_from_left = np.array(from_left)
+    np_from_right = np.array(from_right)
+    return (np_from_left * np_from_right).tolist()
+
+user_input = [1,2,3,4]
+print(mul_without_itself(user_input))
